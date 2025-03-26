e022c0e3f40f670db779a5f23a60f29099466fe4
EldoSantos2004 <leomikel0515@gmail.com>
2025-03-26 12:15:59 -0600

    movi todo pa

---
+++ CodigosMikel/geometry/point.cpp
@@ -0,0 +1,20 @@
+typedef long long T;
+struct pt {
+  T x,y;
+  pt operator+(pt p) {return {x+p.x, y+p.y};}
+  pt operator-(pt p) {return {x-p.x, y-p.y};}
+  pt operator*(T d) {return {x*d, y*d};}
+  pt operator/(T d) {return {x/d, y/d};}
+};
+  // cross product
+  // positivo si el segundo esta en sentido antihorario
+  // 0 si el angulo es 180
+  // negativo si el segundo esta en sentido horario
+T cross(pt v, pt w) {return v.x*w.y - v.y*w.x;}
+  // dot product
+  // positivo si el angulo entre los vectores es agudo
+  // 0 si son perpendiculares
+  // negativo si el angulo es obtuso
+T dot(pt v, pt w) {return v.x*w.x + v.y*w.y;}
+
+T orient(pt a, pt b, pt c) {return cross(b-a,c-a);}
