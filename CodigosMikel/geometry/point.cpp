1cbbd17fc05a058bc6f398a1551dc0e48077e441
EldoSantos2004 <leomikel0515@gmail.com>
2025-04-24 19:47:15 -0600

    stress

--- CodigosMikel/geometry/point.cpp
+++ CodigosMikel/geometry/point.cpp
@@ -6,15 +6,22 @@ struct pt {
   pt operator*(T d) {return {x*d, y*d};}
   pt operator/(T d) {return {x/d, y/d};}
 };
-  // cross product
-  // positivo si el segundo esta en sentido antihorario
-  // 0 si el angulo es 180
-  // negativo si el segundo esta en sentido horario
+
+// cross product
+// positivo si el segundo esta en sentido antihorario
+// 0 si el angulo es 180
+// negativo si el segundo esta en sentido horario
 T cross(pt v, pt w) {return v.x*w.y - v.y*w.x;}
-  // dot product
-  // positivo si el angulo entre los vectores es agudo
-  // 0 si son perpendiculares
-  // negativo si el angulo es obtuso
+
+// dot product
+// positivo si el angulo entre los vectores es agudo
+// 0 si son perpendiculares
+// negativo si el angulo es obtuso
 T dot(pt v, pt w) {return v.x*w.x + v.y*w.y;}
 
 T orient(pt a, pt b, pt c) {return cross(b-a,c-a);}
+
+T dist(pt a,pt b){
+  pt aux=b-a;
+  return sqrtl(aux.x*aux.x+aux.y*aux.y);
+}
