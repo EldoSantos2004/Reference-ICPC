116b799684f0532998b5bc8aebd49b639524ab9d
arns115 <126129471+arns115@users.noreply.github.com>
2025-04-27 19:13:54 -0600

    Update oset.cpp

--- data_structures/oset.cpp
+++ data_structures/oset.cpp
@@ -22,3 +22,13 @@ template <typename T> using oset = __gnu_pbds::tree<
 
 // check if upperbound or lowerbound does what you want
 // because they give better time.
+
+// to allow repetitions
+#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+// to not allow repetitions
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+//order_of_key(x): number of items are strictly smaller than x
+
+//find_by_order(k) iterator to the kth element
