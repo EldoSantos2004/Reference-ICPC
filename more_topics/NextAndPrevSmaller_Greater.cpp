ee5345b40c36d6b0f847647ea87d469f571d0a26
Erick León <89047409+ErickLeonG@users.noreply.github.com>
2025-05-02 10:05:42 -0600

    Update NextAndPrevSmaller_Greater.cpp

--- more_topics/NextAndPrevSmaller_Greater.cpp
+++ more_topics/NextAndPrevSmaller_Greater.cpp
@@ -1,22 +1,29 @@
-vector<int> nextSmaller(vector<int> a, int n){
+/*
+  Next and Previous Smaller Elements
+  ----------------------------------
+  Given: array a[0..n-1]
+  nextSmaller[i]: index of next element smaller than a[i], or n if none
+  prevSmaller[i]: index of previous element smaller than a[i], or -1 if none
+  Indexing: 0-based
+*/
+vector<int> nextSmaller(vector<int> a, int n) {
   stack<int> s;
-  vector<int> res(n, n);
-  for(int i=0;i<n;i++){
-    while(s.size() && a[s.top()]>a[i]){
-      res[s.top()]=i;
+  vector<int> res(n, n); // default: no smaller to right
+  for (int i = 0; i < n; i++) {
+    while (!s.empty() && a[s.top()] > a[i]) {
+      res[s.top()] = i;
       s.pop();
     }
     s.push(i);
   }
   return res;
 }
-
-vector<int> prevSmaller(vector<int> a, int n){
+vector<int> prevSmaller(vector<int> a, int n) {
   stack<int> s;
-  vector<int> res(n, -1);
-  for(int i=n-1;i>=0;i--){
-    while(s.size() && a[s.top()]>a[i]){
-      res[s.top()]=i;
+  vector<int> res(n, -1); // default: no smaller to left
+  for (int i = n - 1; i >= 0; i--) {
+    while (!s.empty() && a[s.top()] > a[i]) {
+      res[s.top()] = i;
       s.pop();
     }
     s.push(i);
