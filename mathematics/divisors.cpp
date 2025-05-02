39d3cee0d7c890105ab5f371ba977bc9aaff2505
Erick León <89047409+ErickLeonG@users.noreply.github.com>
2025-05-02 12:33:45 -0600

    Update divisors.cpp

--- mathematics/divisors.cpp
+++ mathematics/divisors.cpp
@@ -1,53 +1,36 @@
-long long numberOfDivisors(long long num)
-{
-  long long total = 1;
-  for (int i = 2; (long long)i * i <= num; i++)
-  {
-    if (num % i == 0)
-    {
+ll numberOfDivisors(ll num) {
+  ll total = 1;
+  for (int i = 2; (ll)i * i <= num; i++) {
+    if (num % i == 0){
       int e = 0;
-      do
-      {
+      do{
         e++;
         num /= i;
       } while (num % i == 0);
       total *= e + 1;
     }
   }
-  if (num > 1)
-  {
-    total *= 2;
-  }
+  if (num > 1) total *= 2;
   return total;
 }
 
-long long SumOfDivisors(long long num)
-{
-  long long total = 1;
-
-  for (int i = 2; (long long)i * i <= num; i++)
-  {
-    if (num % i == 0)
-    {
+ll SumOfDivisors(ll num) {
+  ll total = 1;
+  for (int i = 2; (ll)i * i <= num; i++) {
+    if (num % i == 0) {
       int e = 0;
-      do
-      {
+      do {
         e++;
         num /= i;
       } while (num % i == 0);
-
-      long long sum = 0, pow = 1;
-      do
-      {
+      ll sum = 0, pow = 1;
+      do {
         sum += pow;
         pow *= i;
       } while (e-- > 0);
       total *= sum;
     }
   }
-  if (num > 1)
-  {
-    total *= (1 + num);
-  }
+  if (num > 1) total *= (1 + num);
   return total;
 }
