277e353a7ff1b6e0e5010c1107a49f6c9987e565
Erick León <89047409+ErickLeonG@users.noreply.github.com>
2025-05-01 23:35:20 -0600

    Update SegTreeStruct_types.cpp

--- data_structures/SegTreeStruct_types.cpp
+++ data_structures/SegTreeStruct_types.cpp
@@ -1,3 +1,4 @@
+// Sum segment tree
 struct sum_t{
   ll val;
   static const long long null_v = 0;
@@ -11,4 +12,45 @@ struct sum_t{
     return ans;
   }
 };
-// agregar max subarray sum 
+// Min segment tree
+struct min_t{
+  ll val;
+  static const long long null_v = 1e18;
+  
+  min_t(): val(null_v) {}
+  min_t(long long v): val(v) {}
+  
+  min_t operator + (const min_t &a) const {
+    min_t ans;
+    ans.val = min(val, a.val);
+    return ans;
+  }
+};
+// Max segment tree
+struct max_t{
+  ll val;
+  static const long long null_v = -1e18;
+  
+  max_t(): val(null_v) {}
+  max_t(long long v): val(v) {}
+  
+  max_t operator + (const max_t &a) const {
+    max_t ans;
+    ans.val = max(val, a.val);
+    return ans;
+  }
+};
+// GCD segment tree
+struct gcd_t{
+  ll val;
+  static const long long null_v = 0;
+  
+  gcd_t(): val(null_v) {}
+  gcd_t(long long v): val(v) {}
+  
+  gcd_t operator + (const gcd_t &a) const {
+    gcd_t ans;
+    ans.val = gcd(val, a.val);
+    return ans;
+  }
+};
