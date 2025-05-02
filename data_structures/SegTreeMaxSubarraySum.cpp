625bf25c0913759cb8344e00a0cfcc121bc48d9b
Erick León <89047409+ErickLeonG@users.noreply.github.com>
2025-05-02 11:40:54 -0600

    Update SegTreeMaxSubarraySum.cpp

--- data_structures/SegTreeMaxSubarraySum.cpp
+++ data_structures/SegTreeMaxSubarraySum.cpp
@@ -2,14 +2,12 @@ const ll inf=1e18;
 
 struct Node {
 	ll maxi, l_max, r_max, sum;
-
   Node(ll _maxi, ll _l_max, ll _r_max, ll _sum){
     maxi=_maxi;
     l_max=_l_max;
     r_max=_r_max;
     sum=_sum;
   }
-
 	Node operator+(Node b) {
 		return {max(max(maxi, b.maxi), r_max + b.l_max),
       max(l_max, sum + b.l_max), max(b.r_max, r_max + b.sum),
@@ -21,16 +19,13 @@ struct Node {
 struct SegmentTreeMaxSubSum{
   int n; 
   vector<Node> t;
-
   SegmentTreeMaxSubSum(int _n) : n(_n), t(2 * _n, Node(-inf, -inf, -inf, -inf)) {}
-
   void update(int pos, ll val) {
     t[pos += n] = Node(val, val, val, val);
     for (pos>>=1; pos ; pos >>= 1) {
       t[pos] = t[2*pos]+t[2*pos+1];
     }
   }
-  
   Node query(int l, int r) {
     Node node_l = Node(-inf, -inf, -inf, -inf);
     Node node_r = Node(-inf, -inf, -inf, -inf);
