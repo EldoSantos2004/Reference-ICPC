1d382f4eeff353695f2075262fa8ce6dd52aab53
Erick León <89047409+ErickLeonG@users.noreply.github.com>
2025-05-02 12:07:28 -0600

    Update iterating_over_all_subsets.cpp

--- more_topics/iterating_over_all_subsets.cpp
+++ more_topics/iterating_over_all_subsets.cpp
@@ -1,8 +1,7 @@
 for (int mk = 0; mk < (1 << k); mk++) {
-    Ap[mk] = 0;
-    for (int s = mk;; s = (s - 1) & mk) {
-        Ap[mk] += A[s];
-        if (!s)
-            break;
-    }
+  Ap[mk] = 0;
+  for (int s = mk;; s = (s - 1) & mk) {
+    Ap[mk] += A[s];
+    if (!s) break;
+  }
 }
