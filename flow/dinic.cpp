cf1f63a2ccf73a5510e4e8b59d7eff6da21b7c31
Arnau Roger Sole Pi <126129471+arns115@users.noreply.github.com>
2025-09-02 13:40:27 -0600

    Update dinic.cpp

--- flow/dinic.cpp
+++ flow/dinic.cpp
@@ -9,87 +9,87 @@
 
 // to find min cut run bfs from source and find all vertices that can be reached
 // edges between vertices that can be reached and the ones that cant are the min cut
-
 struct FlowEdge {
-    int v, u;
-    long long cap, flow = 0;
-    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+  int v, u;
+  long long cap, flow = 0;
+  FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
 };
 
 struct Dinic {
-    const long long flow_inf = 1e18;
-    vector<FlowEdge> edges;
-    vector<vector<int>> adj;
-    int n, m = 0;
-    int s, t;
-    vector<int> level, ptr;
-    queue<int> q;
+  const long long flow_inf = 1e18;
+  vector<FlowEdge> edges;
+  vector<vector<int>> adj;
+  int n, m = 0;
+  int s, t;
+  vector<int> level, ptr;
+  queue<int> q;
 
-    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
-        adj.resize(n);
-        level.resize(n);
-        ptr.resize(n);
-    }
+  Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+      adj.resize(n);
+      level.resize(n);
+      ptr.resize(n);
+  }
 
-    void add_edge(int v, int u, long long cap) {
-        edges.emplace_back(v, u, cap);
-        edges.emplace_back(u, v, 0);
-        adj[v].push_back(m);
-        adj[u].push_back(m + 1);
-        m += 2;
-    }
+  void add_edge(int v, int u, long long cap) {
+      edges.emplace_back(v, u, cap);
+      edges.emplace_back(u, v, 0);
+      adj[v].push_back(m);
+      adj[u].push_back(m + 1);
+      m += 2;
+  }
 
-    bool bfs() {
-        while (!q.empty()) {
-            int v = q.front();
-            q.pop();
-            for (int id : adj[v]) {
-                if (edges[id].cap - edges[id].flow < 1)
-                    continue;
-                if (level[edges[id].u] != -1)
-                    continue;
-                level[edges[id].u] = level[v] + 1;
-                q.push(edges[id].u);
-            }
-        }
-        return level[t] != -1;
-    }
+  bool bfs() {
+      while (!q.empty()) {
+          int v = q.front();
+          q.pop();
+          for (int id : adj[v]) {
+              if (edges[id].cap - edges[id].flow < 1)
+                  continue;
+              if (level[edges[id].u] != -1)
+                  continue;
+              level[edges[id].u] = level[v] + 1;
+              q.push(edges[id].u);
+          }
+      }
+      return level[t] != -1;
+  }
 
-    long long dfs(int v, long long pushed) {
-        if (pushed == 0)
-            return 0;
-        if (v == t)
-            return pushed;
-        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
-            int id = adj[v][cid];
-            int u = edges[id].u;
-            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
-                continue;
-            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
-            if (tr == 0)
-                continue;
-            edges[id].flow += tr;
-            edges[id ^ 1].flow -= tr;
-            return tr;
-        }
-        return 0;
-    }
+  long long dfs(int v, long long pushed) {
+      if (pushed == 0)
+          return 0;
+      if (v == t)
+          return pushed;
+      for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+          int id = adj[v][cid];
+          int u = edges[id].u;
+          if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+              continue;
+          long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+          if (tr == 0)
+              continue;
+          edges[id].flow += tr;
+          edges[id ^ 1].flow -= tr;
+          return tr;
+      }
+      return 0;
+  }
 
-    long long flow() {
-        long long f = 0;
-        while (true) {
-            fill(level.begin(), level.end(), -1);
-            level[s] = 0;
-            q.push(s);
-            if (!bfs())
-                break;
-            fill(ptr.begin(), ptr.end(), 0);
-            while (long long pushed = dfs(s, flow_inf)) {
-                f += pushed;
-            }
-        }
-        return f;
-    }
+  long long flow() {
+      long long f = 0;
+      while (true) {
+          fill(level.begin(), level.end(), -1);
+          level[s] = 0;
+          q.push(s);
+          if (!bfs())
+              break;
+          fill(ptr.begin(), ptr.end(), 0);
+          while (long long pushed = dfs(s, flow_inf)) {
+              f += pushed;
+          }
+      }
+      return f;
+  }
+
 
   vector<pair<int, int>> minCut(){
     vector<pair<int, int>> ans;
@@ -107,14 +107,12 @@ struct Dinic {
         q.push(edges[id].u);
       }
     }
-    set<pair<int, int>> st;
     for(auto edge: edges){
-      if(vis[edge.v] && !vis[edge.u]) {
-        if(st.count({edge.v, edge.u})) continue;
-        st.insert({edge.v, edge.u});
+      if(vis[edge.v] && !vis[edge.u] && edge.flow > 0) {
         ans.push_back({edge.v, edge.u});
       }
     }
     return ans;
   }
+
 };
