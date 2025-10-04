59c58bbf854539e2dc5f6dc1f7f9cb4deee711d1
Arnau Roger Sole Pi <126129471+arns115@users.noreply.github.com>
2025-10-03 22:16:14 -0600

    More explanation on usage of random number generatort
    Refactor random number generator initialization and usage.

--- more_topics/randomNumberGenerators.cpp
+++ more_topics/randomNumberGenerators.cpp
@@ -1,12 +1,17 @@
 //to avoid hacks
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 mt(chrono::steady_clock::now().time_since_epoch().count());
 //you can also just write seed_value if hacks are not an issue
 
 // rng() for generating random numbers between 0 and 2<<31-1
 
 // for generating numbers with uniform probability in range
-uniform_int_distribution<int>(0, n)(rng)
+uniform_int_distribution<int> rng(0, numeric_limits<int>::max());
+
+// then for generating random values just do 
+int random_value = rng(mt);
+
+
 std::normal_distribution<> normal_dist(mean, 2)
 exponential_distribution
 
