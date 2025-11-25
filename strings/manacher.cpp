16a1db1d37c93070a908ea7d558812fb797422bc
Arnau Roger Sole Pi <126129471+arns115@users.noreply.github.com>
2025-11-25 03:22:19 -0600

    Update manacher.cpp

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -44,6 +44,33 @@ vector<int> manacher(string s)
   return vector<int>(begin(res) + 1, end(res) - 1);
 }
 
+// example use. Longest palindrome that ends at each position
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  string s; cin >> s;
+  int n = sz(s);
+  auto man = manacher(s);
+  vector<int> a(n), b(n);
+  // a[i] is how many odd palindromes - 1 with this center 
+  // b[i] is how many even palindromes with this center + 1 as 
+  for(int i = 0; i < sz(man); i += 2){
+    a[i / 2] = (man[i] - 1) / 2;
+    if(i) b[i / 2] = (man[i - 1]) / 2;
+  }
+  vector<int> ans(n + 1, 1);
+  for(int i = 0; i < n; i++){
+    ans[i + a[i]] = max(ans[i + a[i]], a[i] * 2 + 1);
+    if(b[i])ans[i + b[i] - 1] = max(ans[i + b[i] - 1], b[i] * 2);
+  }
+  for(int i = n - 2; i >= 0; i--){
+    ans[i] = max(ans[i], ans[i + 1] - 2);
+  }
+  for(int i = 0; i < n; i++){
+    cout << ans[i] << " ";
+  }
+  cout << endl;
+}
+
 // usage
 // vector<int> p = manacher("abacaba");
 // this will return {2, 1, 4, 1, 2, 1, 8, 1, 2, 1, 4, 1, 2}
